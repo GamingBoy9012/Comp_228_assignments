c6bcb0b6c25a1116a4df4a4c94fe59ecf3992345
danielding1999-prog <danielding1999@gmail.com>
2026-09-23 16:01:39 -0400

    Make changes in A_Calculator line 12

--- Assignment1_Dhruvin_Suvagiya_Fall2026/src/com/va/assign1/A_Calculator.java
+++ Assignment1_Dhruvin_Suvagiya_Fall2026/src/com/va/assign1/A_Calculator.java
@@ -9,7 +9,7 @@ public class A_Calculator {
 	//metjod for calculating average
 	public double average(double mark1,double mark2, double mark3, double mark4)
 	{
-		return (mark1 + mark2 + mark3 + mark4)/4 ;
+		return (mark1 + mark2 + mark3 + mark4)/3 ;
 	}
 	
 	//method for getting the Grade according based on the average
